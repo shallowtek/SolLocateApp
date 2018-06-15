5f68dee52b39b0c932836a061083512eee2fff6c
Matt Shallow <mattshallow@gmail.com>
2018-06-15 13:00:35 +0100

    Location Change

---
+++ LocateApp/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocateApp.Models
+{
+    public class Location
+    {
+
+        public string Eircode { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
